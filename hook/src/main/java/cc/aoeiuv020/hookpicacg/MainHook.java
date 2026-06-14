570b0077e9d88920edda30c10f3c6dbd9fe34139
夜棂依 <zhzhu722@reiyy.com>
2026-06-14 21:45:48 +0800

    移除启用“改善闪退测试版”时的阅读页广告

--- hook/src/main/java/cc/aoeiuv020/hookpicacg/MainHook.java
+++ hook/src/main/java/cc/aoeiuv020/hookpicacg/MainHook.java
@@ -7,6 +7,7 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -136,5 +137,36 @@ public class MainHook implements IXposedHookLoadPackage {
                 }
             }
         });
+        XposedHelpers.findAndHookMethod(
+            "com.picacomic.fregata.adapters.a",
+            lpparam.classLoader,
+            "getView",
+            int.class,
+            View.class,
+            ViewGroup.class,
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    BaseAdapter adapter = (BaseAdapter) param.thisObject;
+                    int position = (Integer) param.args[0];
+                    int viewType = adapter.getItemViewType(position);
+                    if (viewType == 2) {
+                        ViewGroup parent = (ViewGroup) param.args[2];
+                        Context ctx = parent.getContext();
+                        View convertView = (View) param.args[1];
+                        View emptyView;
+                        // View复用
+                        if (convertView != null && "PICA_AD_KILLER".equals(convertView.getTag())) {
+                            emptyView = convertView; 
+                        } else {
+                            emptyView = new View(ctx);
+                            emptyView.setTag("PICA_AD_KILLER");
+                            // 保留一点，避免影响分页和章节切换
+                            emptyView.setLayoutParams(new android.widget.AbsListView.LayoutParams(1, 1));
+                        }
+                        param.setResult(emptyView);
+                    }
+                }
+            });
     }
 }
